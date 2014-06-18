c10752ee93c7db26126547fc39c15fb754c1e4dc
DLBurton <burtondarren@hotmail.co.uk>
2014-06-18 16:58:32 +0100

    Corrections

--- plots/Nick Wardle_files/FvwowXWDUyX.js
+++
@@ -1,5 +0,0 @@
-/*!CK:604757673!*//*1388980488,178158885*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["H+CL8"]); }
-
-__d("legacy:autoset-timezone",["AutosetTimezone"],function(a,b,c,d){var e=b('AutosetTimezone');a.AutosetTimezone=e;a.tz_calculate=e.calculateTimezone;a.tz_autoset=e.setTimezone;},3);
